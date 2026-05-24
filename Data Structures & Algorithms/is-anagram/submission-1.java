3d5f14acaac57e94c0b65dd73f5f5e7f5c5e97ea
Lenka Chandini <141472654+LenkaChandini@users.noreply.github.com>
2026-05-24 13:17:39 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       // char[] sChars = s.toCharArray();
+        //char[] tChars = t.toCharArray();
+
+        //Arrays.sort(sChars);
+        //Arrays.sort(tChars);
+        //return Arrays.equals(sChars, tChars);
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char x : s.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+            for (char x : t.toCharArray()) {
+            count.put(x, count.getOrDefault(x, 0) - 1);
+            }
+             for (int val : count.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
