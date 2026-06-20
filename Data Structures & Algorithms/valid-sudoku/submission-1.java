427f498fc37fd4564e6b0ce12261968bbcd31e96
Lenka Chandini <141472654+LenkaChandini@users.noreply.github.com>
2026-06-20 14:49:42 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean [][] row = new boolean[9][9];
+        boolean [][] cols = new boolean[9][9]; 
+        boolean [][] box = new boolean[9][9];
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int num = board[i][j] - '1';
+                    int boxIn = (i/3) *3 + (j/3);
+
+                    if (row[i][num] || cols[j][num] || box[boxIn][num]){
+                        return false;
+                    }
+                    row[i][num] = cols[j][num] = box[boxIn][num] = true;
+                }
+            }
+        }
+        return true;
+    }
+}
